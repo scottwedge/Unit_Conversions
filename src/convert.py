d661ec3adf0e016d220cd4e3d5d20da4a875d779
RoyGBivDash <ks33@hawaii.edu>
2015-10-14 18:17:53 -1000

    Functions to convert units.

---
+++ src/convert.py
@@ -0,0 +1,11 @@
+def km_2_mi(value):
+    return value / 1.609
+
+def mi_2_km(value):
+    return vale * 1.609
+
+def yrs_2_min(value):
+	return value * 365 * 24 * 60
+
+def min_2_yrs(value):
+	return value / (365 * 24 * 60)
