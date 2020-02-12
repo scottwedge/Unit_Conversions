d82c8ed446350e65abc0e5e71c4be9f3c3ff862f
swedge <scott.wedge@gmail.com>
2020-02-12 14:22:35 -0500

    Fix typos and function calls

--- run_tests.py
+++ run_tests.py
@@ -1,26 +1,25 @@
 import unittest
-from src.convert import kilometers_to_miles, miles_to_kilometers, \
-	years_to_minutes, minutes_to_years
+from src.convert import km_2_mi, mi_2_km, yrs_2_min, min_2_yrs
 
 class TestConvert(unittest.TestCase):
 
 	def test_km_2_mi(self):
-		actual = kilometers_to_miles(1)
+		actual = km_2_mi(1)
 		expected = 0.621 # From Brian, from Google
 		self.assertAlmostEqual(actual, expected, delta = 0.01)
 
 	def test_mi_2_km(self):
-		actual = miles_to_kilometers(1)
+		actual = mi_2_km(1)
 		expected = 1.609
 		self.assertAlmostEqual(actual, expected, delta = 0.01)
 
 	def test_yrs_2_min(self):
-		self.assertEqual(525600, years_to_minutes(1))
+		self.assertEqual(525600, yrs_2_min(1))
 
 	def test_min_2_yrs(self):
-		self.assertEqual(1, minutes_to_years(525600))
+		self.assertEqual(1, min_2_yrs(525600))
 
 # To run the function this is needed here.
 
 if __name__ == '__main__':
-	uniittest.main()
+	unittest.main()
--- src/convert.py
+++ src/convert.py
@@ -2,7 +2,7 @@ def km_2_mi(value):
     return value / 1.609
 
 def mi_2_km(value):
-    return vale * 1.609
+    return value * 1.609
 
 def yrs_2_min(value):
 	return value * 365 * 24 * 60
